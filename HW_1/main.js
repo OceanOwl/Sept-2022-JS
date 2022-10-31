538badeb83c0a137e820f1d8986f01f422068fd1
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2022-10-31 22:43:08 +0100

    Sept_2022_JS

---
+++ HW_1/main.js
@@ -0,0 +1,55 @@
+let hello = 'hello';
+console.log(hello);
+
+let owu = 'owu';
+console.log(owu);
+
+let com = 'com';
+console.log(com);
+
+let ua = 'ua';
+console.log(ua);
+
+let num = 1;
+console.log(num);
+
+let numF = 10;
+console.log(numF);
+
+let numS = -999;
+console.log(numS);
+
+let numT = 123;
+console.log(numT);
+
+let numFourth = 3.14;
+console.log(numFourth);
+
+let numFifth = 2.7;
+console.log(numFifth);
+
+let numSi = 16;
+console.log(numS);
+
+let tru = 'true';
+console.log(tru);
+
+let fal = 'false';
+console.log(fal);
+
+let firstName = 'Yurii';
+let middleName = 'Lisetskyi';
+let lastName = 'Andr';
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+
+
