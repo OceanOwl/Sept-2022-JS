9f9f4e94a0ad1427c5d9423005f46d6db641a0e2
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2022-11-02 22:36:21 +0100

    HW_2

---
+++ HW_2/main.js
@@ -0,0 +1,212 @@
+//- Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+let arr = [193, 65, 43, 12, 23, 34, 45, 56, 67, 89];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+
+
+
+
+//- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+let book1 = {
+    title: 'title',
+    pageCount: 123,
+    genre: 'fiction',
+}
+let book2 = {
+    title: 'main',
+    pageCount: 234,
+    genre: 'drama',
+}
+let book3 = {
+    title: 'main1',
+    pageCount: 345,
+    genre: 'poetry',
+}
+console.log(book1, book2, book3);
+
+
+
+
+
+
+//- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+let book4 = {
+    title: 'title',
+    pageCount: 123,
+    genre: 'fiction',
+    authors: {
+        name: 'someOne',
+        age: 30,
+
+    }
+}
+let book5 = {
+    title: 'main',
+    pageCount: 234,
+    genre: 'drama',
+    authors: {
+        name: 'someOne',
+        age: 30,
+
+    }
+}
+let book6 = {
+    title: 'main1',
+    pageCount: 345,
+    genre: 'poetry',
+    authors: {
+        name: 'someOne',
+        age: 30,
+
+    }
+}
+
+console.log(book4,book5, book6)
+
+
+
+
+// Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+    let user1 = {
+        name: 'Jan',
+        age: 20,
+        surName: 'pupkin',
+        status: 'true',
+        id: 1,
+        educationLevel: 'high',
+        language: 'Eng',
+        password: 'password',
+        gender: 'M',
+        other: 1
+
+    }
+let user2 = {
+    name: 'Jan',
+    age: 20,
+    surName: 'pupkin',
+    status: 'true',
+    id: 1,
+    educationLevel: 'high',
+    language: 'Eng',
+    password: 'password',
+    gender: 'M',
+    other: 1
+
+}
+let user3 = {
+    name: 'Jan',
+    age: 20,
+    surName: 'pupkin',
+    status: 'true',
+    id: 1,
+    educationLevel: 'high',
+    language: 'Eng',
+    password: 'password',
+    gender: 'M',
+    other: 1
+
+}
+let user4 = {
+    name: 'Jan',
+    age: 20,
+    surName: 'pupkin',
+    status: 'true',
+    id: 1,
+    educationLevel: 'high',
+    language: 'Eng',
+    password: 'password',
+    gender: 'M',
+    other: 1
+
+}
+let user5 = {
+    name: 'Jan',
+    age: 20,
+    surName: 'pupkin',
+    status: 'true',
+    id: 1,
+    educationLevel: 'high',
+    language: 'Eng',
+    password: 'password',
+    gender: 'M',
+    other: 1
+
+}
+let user6 = {
+    name: 'Jan',
+    age: 20,
+    surName: 'pupkin',
+    status: 'true',
+    id: 1,
+    educationLevel: 'high',
+    language: 'Eng',
+    password: 'password',
+    gender: 'M',
+    other: 1
+
+}
+let user7 = {
+    name: 'Jan',
+    age: 20,
+    surName: 'pupkin',
+    status: 'true',
+    id: 1,
+    educationLevel: 'high',
+    language: 'Eng',
+    password: 'password',
+    gender: 'M',
+    other: 1
+
+}
+let user8 = {
+    name: 'Jan',
+    age: 20,
+    surName: 'pupkin',
+    status: 'true',
+    id: 1,
+    educationLevel: 'high',
+    language: 'Eng',
+    password: 'password',
+    gender: 'M',
+    other: 1
+
+}
+let user9 = {
+    name: 'Jan',
+    age: 20,
+    surName: 'pupkin',
+    status: 'true',
+    id: 1,
+    educationLevel: 'high',
+    language: 'Eng',
+    password: 'password',
+    gender: 'M',
+    other: 1
+
+}
+let user10 = {
+    name: 'Jan',
+    age: 20,
+    surName: 'pupkin',
+    status: 'true',
+    id: 1,
+    educationLevel: 'high',
+    language: 'Eng',
+    password: 'password',
+    gender: 'M',
+    other: 1
+
+}
+
+let users = {user1, user2 , user3, user4, user5, user6, user7, user8, user9, user10}
+console.log(users);
+console.log(users.user1.password, users.user2.password, users.user3.password, users.user4.password, users.user5.password, users.user6.password, users.user7.password, users.user8.password, users.user9.password, users.user10.password);
