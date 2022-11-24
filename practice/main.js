b435a21d779b6c6661c0dd282e2abc9fe5d0d82a
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2022-11-24 11:26:30 +0100

    justSomePractice

---
+++ practice/main.js
@@ -0,0 +1,36 @@
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+// let user =
+//     users
+//         .map((user, index) => ({id: index + 1, name: user.name, status: user.status}))
+//         .filter(item => item.id % 2 === 0)
+//         .forEach(item => console.log(item));
+
+// let filter = users.filter(value => value.age > 29 && !value.status);
+// console.log(filter);
+
+// console.log(users.sort(function (a, b) {
+//     return a.name.length - b.name.length
+//
+// }));
+
+// console.log(users.sort(function (a, b) {
+//     if (a.name < b.name)
+//         return -1;
+//     if (a.name > b.name)
+//         return 1;
+//     if (a.name === b.name)
+//         return 0;
+//
+// }));
